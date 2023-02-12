3353f5cde8b8f366b7d24400578ce51fdf806570
Fatemah <f.thenyan@gmail.com>
2023-02-12 15:09:26 +0300

    home sections updated

--- Music-Project/src/components/Home.tsx
+++ Music-Project/src/components/Home.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+
 import bookMusician from '../assets/bookMusician.jpeg'
 import musicalInstruments from '../assets/musicalInstruments.jpeg'
 import musicEducation from '../assets/musicEducation.jpeg'
+
+import About from './About&Con/About';
 
 export default function Home() {
   const navigate = useNavigate()
   const SignUp =()=>{
     navigate("/signUp")    
+  }
+  const About =()=>{
+    navigate("/about")    
   }
   const MusicEducation =()=>{
     navigate("/musicEducation")    
@@ -20,17 +26,35 @@ export default function Home() {
   }
   return (
     <div>
-      {/*----------header----------*/}
-        <header className='headerBg'>
-          <div className='headerContent'>
-            <div>Music <i>for</i> 
-            <br/> Everyone</div> 
-            <button onClick={SignUp}> JOIN US</button>
-          </div>    
-        </header>
+      {/*----------HEADER----------*/}
+      <header className='headerBg'>
+        <div className='headerContent'>
+          <div>Music <i>for</i> 
+          <br/> Everyone</div> 
+          <button onClick={SignUp}> JOIN US</button>
+        </div>    
+      </header>
+
+      {/*----------MAIN----------*/} 
+      <main>
       
-      {/*----------service section----------*/}
-        <main>
+        {/*----------about section----------*/}
+        <section className='aboutSectionBg'>
+          <div className='aboutSection'>
+            <h2> <i>Welcome to </i> The Musical Community</h2>
+            <p>I think music in itself is healing. It's an explosive expression of humanity. It's something we are all touched by. 
+                No matter what culture we're from, everyone loves music.
+            </p>
+            <p>I think music in itself is healing. It's an explosive expression of humanity. It's something we are all touched by. 
+              No matter what culture we're from, everyone loves music.
+            </p>
+            <button onClick={About}>READ MORE</button>
+          </div>   
+        </section>
+
+        {/*----------service divs----------*/}   
+        <section className='serviceSectionContainer'>
+
           <div className='serviceSection'>
             <img src={musicEducation} alt="" />
             <div className='serviceContent'>
@@ -41,10 +65,10 @@ export default function Home() {
               <p>I think music in itself is healing. It's an explosive expression of humanity. It's something we are all touched by. 
                 No matter what culture we're from, everyone loves music.
               </p>
-             
               <button onClick={MusicEducation}>READ MORE</button>
             </div>
           </div>
+
           <div className='serviceSection'>
             <div className='serviceContent right'>
               <h2>Book A Musician</h2>
@@ -58,6 +82,7 @@ export default function Home() {
             </div>
             <img src={bookMusician} alt="" />
           </div>
+
           <div className='serviceSection'>
             <img src={musicalInstruments} alt="" />
             <div className='serviceContent'>
@@ -71,13 +96,19 @@ export default function Home() {
               <button onClick={MusicalInstruments}>READ MORE</button>
             </div>
           </div>
+        </section>   
 
-        </main>
-
-        <section>
-
+        {/*----------news letter section----------*/}
+        <section className='newsLetterSectionContainer'>
+          <div className='newsLetterSection'>
+            <h2>NEWS LETTER</h2>
+            <p>Keep up our latest news. Subscribe our news letter </p>
+            <input type='email' placeholder='Enter Your Email..' ></input>
+            <button>SUBSCRIBE</button>
+          </div> 
         </section>
 
+      </main>
     </div>
   )
 }
