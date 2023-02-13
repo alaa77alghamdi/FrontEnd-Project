18f0471df34883601c0c01d88e1e16042efbba57
Ameerah Dhafer <ameerahalshihry@gmail.com>
2023-02-13 14:40:51 +0300

    update name of the instrument in local storage

--- Music-Project/src/components/ServicesComp/ElementComp/StoreCard.tsx
+++ Music-Project/src/components/ServicesComp/ElementComp/StoreCard.tsx
@@ -26,7 +26,7 @@ import {
       if (localStorage.getItem("name") != null){
           //redirect to profile
           navigate('/profile')
-          localStorage.setItem("name", name)
+          localStorage.setItem("nameInstrument", name)
           localStorage.setItem("img", img)
           localStorage.setItem("desc", desc)
           localStorage.setItem("price", price)
